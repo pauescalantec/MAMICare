ae96c49568e06e12945c263425c31b13c1e7c079
pauescalantec <pauescalante8@gmail.com>
2015-10-22 23:42:27 -0500

    Avance a interfaz, acceso a fotos

---
+++ MAMICare/MAMICare/ViewControllerNuevoPaciente.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerNuevoPaciente.h
+//  MAMICare
+//
+//  Created by Pau Escalante on 10/22/15.
+//  Copyright © 2015 Paulina´s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerNuevoPaciente : UIViewController
+
+@end
