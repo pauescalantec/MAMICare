4d74357ce193729d793e798a3387e2eb8d5919d2
Luis-Lamat <beto_alaska@hotmail.com>
2015-11-18 13:08:29 -0600

    Starts PatientPhysicalForm Viewcontrollers

---
+++ MAMICare/MAMICare/PatientPhysicalFormViewController.h
@@ -0,0 +1,16 @@
+//
+//  PatientPhysicalFormViewController.h
+//  MAMICare
+//
+//  Created by Luis Alberto Lamadrid on 11/18/15.
+//  Copyright © 2015 Paulina´s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "StatusReport.h"
+
+@interface PatientPhysicalFormViewController : UIViewController
+
+@property(nonatomic, strong) StatusReport *statusReport;
+
+@end
---
+++ MAMICare/MAMICare/PatientPhysicalFormViewController.m
@@ -0,0 +1,37 @@
+//
+//  PatientPhysicalFormViewController.m
+//  MAMICare
+//
+//  Created by Luis Alberto Lamadrid on 11/18/15.
+//  Copyright © 2015 Paulina´s. All rights reserved.
+//
+
+#import "PatientPhysicalFormViewController.h"
+
+@interface PatientPhysicalFormViewController ()
+
+@end
+
+@implementation PatientPhysicalFormViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
