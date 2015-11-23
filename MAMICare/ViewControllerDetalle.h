0391c0695bc3302262bac3eb319377eb56f31eb3
Luis-Lamat <beto_alaska@hotmail.com>
2015-11-22 20:37:35 -0600

    Adds Outlets to DetailViewController

--- MAMICare/ViewControllerDetalle.h
+++ MAMICare/ViewControllerDetalle.h
@@ -11,16 +11,29 @@
 
 @interface ViewControllerDetalle : UIViewController
 
+// outlets
 @property (nonatomic, strong) Patient *patient;
-
 @property (nonatomic, strong) NSString *strSegue;
-
 @property (nonatomic, strong) NSString *strNuevo;
-
 @property (nonatomic, strong) UIBarButtonItem *Cancel;
-
 @property (strong, nonatomic) IBOutlet UIButton *btnStart;
 
+@property (weak, nonatomic) IBOutlet UILabel *lblFirstName;
+@property (weak, nonatomic) IBOutlet UILabel *lblLastNames;
+@property (weak, nonatomic) IBOutlet UILabel *lblAge;
+@property (weak, nonatomic) IBOutlet UILabel *lblStreet;
+@property (weak, nonatomic) IBOutlet UILabel *lblNeighborhood;
+@property (weak, nonatomic) IBOutlet UILabel *lblCity;
+@property (weak, nonatomic) IBOutlet UILabel *lblState;
+@property (weak, nonatomic) IBOutlet UILabel *lblPostalCode;
+@property (weak, nonatomic) IBOutlet UILabel *lblCountry;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblEmail;
+@property (weak, nonatomic) IBOutlet UILabel *lblCURP;
+
+
+
+// action
 - (void)btnCancelar:(UIBarButtonItem *)sender;
 
 @end
